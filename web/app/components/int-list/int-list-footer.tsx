697c1de73f8e7ad8fb135ec81d92b2450e146682
rlim2 <usprinterhihi1@gmail.com>
2025-12-24 19:23:16 -0800

    intlist base ui

--- web/app/components/int-list/int-list-footer.tsx
+++ web/app/components/int-list/int-list-footer.tsx
@@ -4,10 +4,14 @@ export default function IntListFooter () {
     return (
         <>
         <div className="absolute bottom-0 px-1 w-full h-[10%] border-t-2 flex items-center gap-1">
+            
+            <input className="px-2 py-[1.1px] text-xs 
+            bg-background pixel-borders w-30 text-border
+            focus:border-accent placeholder:text-border/75" placeholder="ign" />
+            <input className="px-2 py-[1.1px] text-xs 
+            bg-background pixel-borders text-border
+            focus:border-accent placeholder:text-border/75" placeholder="reason" />
             <button className="pixel-btn-sm">+</button>
-            <input className="flex-1 px-2 py-[1.1px] text-xs 
-            bg-background border pixel-borders
-            focus:border-accent placeholder:text-border/60" placeholder="Add player..." />
         </div>
         </>
     )
