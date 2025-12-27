d213338b5873fa21023cef9d666ce9c65f029050
rlim2 <usprinterhihi1@gmail.com>
2025-12-27 00:27:04 -0800

    small ui stuff

--- web/app/components/main-card/profile-section.tsx
+++ web/app/components/main-card/profile-section.tsx
@@ -14,8 +14,8 @@ export default function ProfileSection() {
                         className="pixel-borders-dome rounded-sm object-cover"
                     />
                 </div>
-                <div className="w-[60%] flex items-center pixel-borders">
-                    <span className="text-center text-foreground w-full leading-tight bg-border/80">Rosie</span>
+                <div className="w-[60%] flex items-center ">
+                    <span className="text-center text-accent w-full leading-tight bg-background pixel-borders">Rosie</span>
                 </div>
             </div>
             <div className="w-full px-1 pt-[2px] main-text flex flex-col -mt-[5px]">
@@ -23,7 +23,7 @@ export default function ProfileSection() {
                     <span className="font-bold">bday</span>
                     <div className="">[10/07]</div>
                 </div>
-                <div className="pixel-borders relative -mt-[3px]">
+                <div className="pixel-borders relative -mt-[3px] bg-background">
                     <div className="h-[3px] "></div>
                     <div className="h-[1px] absolute -top-[0px] w-[77%] bg-accent left-[2.3px]"></div>
                     <div className="h-[2px] absolute top-[2.5px] w-[74%] bg-accent left-[5.5px]"></div>
@@ -33,29 +33,29 @@ export default function ProfileSection() {
             </div>
             <div className="flex gap-1 py-[2] px-1">
                 <a href="https://www.twitch.tv/rosie" target="_blank" rel="noopener noreferrer">
-                    <div className="p-0.5 bg-foreground flex items-center justify-center w-[18] h-5">
-                        <i className="hn hn-twitch text-border hover:animate-bounce pixel-borders" />
+                    <div className="p-0.5 flex items-center justify-center w-[18] h-5">
+                        <i className="hn hn-twitch text-border hover:animate-bounce pixel-borders bg-background" />
                     </div>
                 </a>
                 <a href="https://x.com/ttvrosie" target="_blank" rel="noopener noreferrer">
-                    <div className="p-0.5 bg-foreground flex items-center justify-center w-[18] h-5">
-                        <i className="hn hn-twitter text-border hover:animate-bounce pixel-borders" />
+                    <div className="p-0.5 flex items-center justify-center w-[18] h-5">
+                        <i className="hn hn-twitter text-border hover:animate-bounce pixel-borders bg-background" />
                     </div>
                 </a>
 
                 <a href="https://www.tiktok.com/@ttvrosie" target="_blank" rel="noopener noreferrer">
-                    <div className="p-0.5 bg-foreground flex items-center justify-center w-[18] h-5">
-                        <i className="hn hn-tiktok text-border hover:animate-bounce pixel-borders" />
+                    <div className="p-0.5 flex items-center justify-center w-[18] h-5">
+                        <i className="hn hn-tiktok text-border hover:animate-bounce pixel-borders bg-background" />
                     </div>
                 </a>
                 <a href="https://www.youtube.com/@ttvrosie" target="_blank" rel="noopener noreferrer">
-                    <div className="p-0.5 bg-foreground  flex items-center justify-center w-[18] h-5">
-                        <i className="hn hn-youtube text-border hover:animate-bounce pixel-borders" />
+                    <div className="p-0.5 flex items-center justify-center w-[18] h-5">
+                        <i className="hn hn-youtube text-border hover:animate-bounce pixel-borders bg-background" />
                     </div>
                 </a>
                 <a href="https://vgen.co/rosieuna" target="_blank" rel="noopener noreferrer">
-                    <div className="p-0.5 bg-foreground flex items-center justify-center w-[18] h-5">
-                        <i className="hn hn-highlight text-border hover:animate-bounce pixel-borders" />
+                    <div className="p-0.5 flex items-center justify-center w-[18] h-5">
+                        <i className="hn hn-highlight text-border hover:animate-bounce pixel-borders bg-background" />
                     </div>
                 </a>
             </div>
