5a185246d1678ad68f97930cbbec18cc3c4478c7
Josh <joshiqua@gmail.com>
2025-12-27 19:00:08 -0800

    added scrolling utility function for horizonatal scrolling without  needing to hold down shift for tabs

--- web/app/components/shared/tabs.tsx
+++ web/app/components/shared/tabs.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { useRef } from "react";
+
 interface TabProps {
     tabs: {
         title: string;
@@ -11,10 +13,23 @@ interface TabProps {
 }
 
 export default function Tabs({ tabs, activeTab, setActiveTab }: TabProps) {
+    const scrollRef = useRef<HTMLDivElement>(null);
+
+    const handleWheel = (e: React.WheelEvent) => {
+        if (scrollRef.current) {
+            e.preventDefault();
+            scrollRef.current.scrollLeft += e.deltaY;
+        }
+    };
+
     return (
-        <div className="flex items-center justify-start gap-0.5 h-5 relative ml-0.5 pb-[1px]">
-            {tabs.map((tab) => (
-                <div className="relative cursor-pointer" key={tab.title} onClick={() => setActiveTab(tab)}>
+        <div 
+            ref={scrollRef}
+            onWheel={handleWheel}
+            className="flex items-center justify-start gap-0.5 h-5.5 relative ml-0.5 pb-[1px] overflow-x-auto overflow-y-visible scrollbar-hide"
+        >
+            {tabs.map((tab, index) => (
+                <div className="relative cursor-pointer" key={`${tab.title}-${index}`} onClick={() => setActiveTab(tab)}>
                     {activeTab.title === tab.title ? (
                         <>
                             <div className="bg-white absolute mt-0.5 w-[calc(100%_-_4px)] left-0.5 pointer-events-none rounded-t-sm h-0.5 border-accent!"></div>
