137cdb10b0209c0a1e00639e37affe8885eafe8c
rlim2 <usprinterhihi1@gmail.com>
2026-01-10 23:43:59 -0800

    new opgg url

--- web/app/opgg/page.tsx
+++ web/app/opgg/page.tsx
@@ -4,7 +4,7 @@ import { useEffect } from "react"
 
 export default function Opgg() {
     useEffect(() => {
-        window.location.replace("https://www.deeplol.gg/strm/rosie")
+        window.location.replace("https://op.gg/ko/lol/multisearch/na?summoners=rosie%23twtv%2Cuguysrsobad%23wtf%2Coldmanhands%23loser%2CINTERNETGIRL%23LOSER%2C%EC%9B%90%EC%88%AD%EC%9D%B4%EB%8F%84%EB%82%98%EB%AC%B4%EC%97%90%EC%84%9C%EB%96%A8%EC%96%B4%EC%A7%88%EB%95%8C%EA%B0%80%EC%9E%87%EB%8B%A4%23monk")
     }, [])
 
     return null
