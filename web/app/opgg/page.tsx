52b97a7f84aeffc6ea0002f69a8968336d11b4e1
rlim2 <usprinterhihi1@gmail.com>
2026-03-01 12:08:24 -0800

    opggupdateagain

--- web/app/opgg/page.tsx
+++ web/app/opgg/page.tsx
@@ -4,7 +4,7 @@ import { useEffect } from "react"
 
 export default function Opgg() {
     useEffect(() => {
-        window.location.replace("https://op.gg/ko/lol/multisearch/na?summoners=rosie%23twtv%2Cttvrosie%23unc%2CINTERNETGIRL%23LOSER%2Cduoanyone%23addme")
+        window.location.replace("https://op.gg/lol/multisearch/na?summoners=rosie%23twtv%2Cttvrosie%23unc%2Cduoanyone%23addme")
     }, [])
 
     return null
