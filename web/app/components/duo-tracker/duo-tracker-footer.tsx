92a23f8951c004e3629c81d6a2519ae6215239d8
rlim2 <usprinterhihi1@gmail.com>
2026-03-01 17:03:53 -0800

    ui for logout footer

--- web/app/components/duo-tracker/duo-tracker-footer.tsx
+++ web/app/components/duo-tracker/duo-tracker-footer.tsx
@@ -16,15 +16,31 @@ export default function DuoTrackerFooter({ isRosie, onOpenOpgg }: DuoTrackerFoot
         console.log("Add duo:", { name, result })
         setName("")
     }
+    const openOPGG = () => {
+        // Redirects to opgg in new window
+        window.open('https://op.gg/ko/lol/summoners/na/DUO%20ANYONE-ADDME', '_blank');
+    };
 
     if (!isRosie) {
         return (
-            <div className="px-[var(--spacing-sm)] w-full h-[1.75rem] flex items-center border-t-2 gap-[var(--spacing-sm)]">
+            <div className="px-[var(--spacing-md)] w-full h-[1.75rem] flex items-center border-t-2 gap-[var(--spacing-md)]">
                 <button
-                    className="main-text hover:underline truncate"
-                    onClick={onOpenOpgg}
+                    className="main-text truncate"
+                    onClick={openOPGG}
                 >
-                    add opgg + dm to play
+                    add
+                </button>
+                <button
+                    onClick={openOPGG}
+                    className="pixel-borders pixel-btn-white hover:bg-background! cursor-pointer select-none"
+                >
+                    opgg
+                </button>
+                <button
+                    className="main-text truncate"
+                    onClick={openOPGG}
+                >
+                    + dm to play
                 </button>
             </div>
         )
