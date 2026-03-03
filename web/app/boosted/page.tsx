1d5e960cffdf24d3cc9fb046ad62952594b9b458
rlim2 <usprinterhihi1@gmail.com>
2026-03-03 11:28:46 -0800

    imgs

---
+++ web/app/boosted/page.tsx
@@ -0,0 +1,11 @@
+"use client"
+
+import { useEffect } from "react"
+
+export default function Boosted() {
+    useEffect(() => {
+        window.location.replace("https://imgur.com/a/26fA7oN")
+    }, [])
+
+    return null
+}
