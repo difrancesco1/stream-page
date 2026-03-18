9dd1b447c6f7fb09111043cdc36e2fbf214b8e63
rlim2 <usprinterhihi1@gmail.com>
2026-03-18 12:04:10 -0700

    first page attempt

---
+++ web/app/diao/page.tsx
@@ -0,0 +1,11 @@
+"use client"
+
+import { useEffect } from "react"
+
+export default function Diao() {
+    useEffect(() => {
+        window.location.replace("https://shoutatyourdecks.com/decks/3c5cd907-6145-467e-9729-875ce7296557")
+    }, [])
+
+    return null
+}
