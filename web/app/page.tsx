ca7538a1cd3ea66568292b2116dc6909e51b898b
difrancesco1 <joshiqua@gmail.com>
2026-03-18 01:12:24 -0700

    wrap cardContainer in suspense to read the query param.

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,7 +1,10 @@
+import { Suspense } from "react";
 import CardContainer from "./components/card-container";
 
 export default function Home() {
   return <div className="w-full h-screen bg-background p-[0.325rem] lg:p-[1.825rem] md:p-[1rem]">
-    <CardContainer />
+    <Suspense>
+      <CardContainer />
+    </Suspense>
   </div>;
 }
