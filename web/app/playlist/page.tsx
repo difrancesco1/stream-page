b0f69540151577112442c3be25aea119098da8c9
rlim2 <usprinterhihi1@gmail.com>
2026-03-31 23:33:33 -0700

    playlist

--- web/app/playlist/page.tsx
+++ web/app/playlist/page.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect } from "react"
 
-export default function Opgg() {
+export default function Playlist() {
     useEffect(() => {
         window.location.replace("https://open.spotify.com/playlist/5OUgqXjiG8begRdLpy3sMO?si=0a734e54074c4bf3")
     }, [])
