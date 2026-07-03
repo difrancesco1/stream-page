f948ff83ecb32314966e7b9556a5f8e22306ed38
Josh <joshiqua@gmail.com>
2026-07-03 12:27:06 -0700

    updated opgg link

--- web/lib/vanity-urls.ts
+++ web/lib/vanity-urls.ts
@@ -16,7 +16,7 @@ export const vanityUrls: VanityUrl[] = [
   {
     source: "/opgg",
     destination:
-      "https://op.gg/lol/multisearch/na?summoners=ignisfirebloom%23pass%2Cziggs7%23rosie%2Cduoanyone%23addme%2CNEW+PLAYER%23CHUD",
+    "https://op.gg/lol/multisearch/na?summoners=ignisfirebloom%23pass%2Cziggs7%23rosie%2Chardstuck+plat%23sry%2CNEW+PLAYER%23CHUD",
   },
   {
     source: "/playlist",
